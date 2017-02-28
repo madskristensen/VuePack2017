9f8163ba062791ee107f963dc501068e7fc6087a
Mads Kristensen <post@madskristensen.net>
2017-02-28 15:16:21 -0800

    Updated name

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -8,7 +8,7 @@ namespace VuePack
     static class Vsix
     {
         public const string Id = "4163aa08-9878-4b1c-a1ee-58ab67f7db5b";
-        public const string Name = "Vue.js Pack";
+        public const string Name = "Vue.js Pack 2017";
         public const string Description = @"Contains HTML Intellisense and code snippets for the Vue.js JavaScript library";
         public const string Language = "en-US";
         public const string Version = "1.0";
